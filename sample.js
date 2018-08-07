9829c97dfa99fa3d73943fedda519cf5cea5fe54
Pablo Klaschka <pklaschka@users.noreply.github.com>
2018-08-07 19:25:55 +0200

    Update sample.js
    Removing copyright, changing return to module.exports

--- sample.js
+++ sample.js
@@ -1,27 +1,21 @@
-/*
- * Copyright (c) 2018.  Pablo Klaschka <contact@pabloklaschka.de>.
- */
 const {Text, Ellipse, Color} = require("scenegraph");
 const clipboard = require("clipboard");
 
 /**
- *
  * @param {Selection} selection
  */
 function test(selection) {
     selection.items.forEach(node => {
-        console.log("Moin", node);
+        console.log("Hello world: ", node);
         if (node instanceof Text) {
             clipboard.copyText(node.text);
-            node.text = "Moin";
         } else if (node instanceof Ellipse) {
             node.fill = new Color("#ffaaee");
         }
     });
 }
 
-// @ts-ignore
-return {
+module.exports = {
     commands: {
         test: test
     }
