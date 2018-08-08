11a6b67b0e6d6a10c9c8103a1a0fdba39905950d
pklaschka <pabloklaschka@yahoo.de>
2018-08-08 11:59:01 +0200

    Implemented definitions for scenegraph.Blur

--- types/scenegraph.d.ts
+++ types/scenegraph.d.ts
@@ -90,6 +90,48 @@ declare class Shadow {
 }
 
 declare class Blur {
+    /**
+     * The amount of blur
+     *
+     * (0 - 50)
+     */
+    public blurAmount: number;
+    /**
+     * For background blur effects, the amount to increase or decrease the brightness of the background. Ignored for object blur effects.
+     *
+     * (-50 - 50)
+     */
+    public brightnessAmount: number;
+
+    /**
+     * For background blur effects, the a multiplier on the opacity of the object's fill drawn over top of the blurred background. Useful to create a color tint on top of the blurred background. Does not affect stroke opacity.
+     *
+     * Ignored for object blur effects.
+     *
+     * (0.0 - 1.0)
+     */
+    public fillOpacity: number;
+    /**
+     * If true, renders a background blur effect: all objects beneath the shape are blurred (modulated by brightnessAmount), but the shape itself is still rendered with crisp edges (with its fill modulated by fillOpacity).
+     *
+     * If false, renders an object blur effect: the shape itself is blurred, and objects beneath it are unaffected.
+     */
+    public isBackgroundEffect: boolean;
+
+    /**
+     * If false, the blur effect is not rendered. The user can toggle this via a checkbox in the Properties panel.
+     */
+    public visible: boolean;
+
+    /**
+     * Creates an object blur or background blur effect object with the given properties.
+     * @param blurAmount
+     * @param brightnessAmount
+     * @param fillOpacity
+     * @param visible
+     * @param isBackgroundEffect
+     */
+    constructor(blurAmount: number, brightnessAmount: number, fillOpacity: number, visible?: boolean, isBackgroundEffect?: boolean);
 }
 
 declare class Bounds {
