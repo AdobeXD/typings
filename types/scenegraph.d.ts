e06bc6b5573d2bd564dd377ad60381ebc6a5e3ef
pklaschka <pabloklaschka@yahoo.de>
2018-08-08 12:29:36 +0200

    Implemented definitions for scenegraph.LinearGradientFill

--- types/scenegraph.d.ts
+++ types/scenegraph.d.ts
@@ -204,6 +204,54 @@ declare class Color {
 }
 
 declare class LinearGradientFill {
+    /**
+     * Array of objects representing each color and its position along the gradient line. The position (stop value) is a number 0.0 - 1.0.
+     */
+    public colorStops: Color[];
+
+    /**
+     * X position of the start of the gradient line, as a multiple of the object's bounding box: X=0 indicates the left edge of the bounding box and X=1 indicates the right edge. The gradient line may start or end outside the object's bounding box, so values may be < 0 or > 1.
+     */
+    public startX: number;
+
+    /**
+     * Y position of the start of the gradient line, as a multiple of the object's bounding box: Y=0 indicates the top edge of the bounding box and Y=1 indicates the bottom edge. The gradient line may start or end outside the object's bounding box, so values may be < 0 or > 1.
+     */
+    public startY: number;
+
+    /**
+     * X position of the end of the gradient line, as a multiple of the object's bounding box: X=0 indicates the left edge of the bounding box and X=1 indicates the right edge. The gradient line may start or end outside the object's bounding box, so values may be < 0 or > 1.
+     */
+    public endX: number;
+
+    /**
+     * Y position of the end of the gradient line, as a multiple of the object's bounding box: Y=0 indicates the top edge of the bounding box and Y=1 indicates the bottom edge. The gradient line may start or end outside the object's bounding box, so values may be < 0 or > 1.
+     */
+    public endY: number;
+
+    /**
+     * Create a new LinearGradientFill instance.
+     */
+    public constructor();
+
+    /**
+     * Returns a copy of this instance.
+     */
+    public clone(): LinearGradientFill;
+
+    /**
+     * Returns an array of [startX, startY, endX, endY].
+     */
+    public getEndPoints(): number[];
+
+    /**
+     * Shorthand for setting all four start/endpoint properties.
+     * @param startX
+     * @param startY
+     * @param endX
+     * @param endY
+     */
+    public setEndPoints(startX: number, startY: number, endX: number, endY: number);
 }
 
 declare class RadialGradientFill {
