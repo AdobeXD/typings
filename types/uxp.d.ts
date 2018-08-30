a96cc0498d6bc03ed43be62174d7f6e07b7ec542
pklaschka <pabloklaschka@yahoo.de>
2018-08-31 01:41:27 +0200

    JSDoc for storage.FileSystemProvider

--- types/uxp.d.ts
+++ types/uxp.d.ts
@@ -209,29 +209,90 @@ declare module storage {
         public static isFile(entry: any): boolean;
     }
 
-    declare export class FileSystemProvider {
+    /**
+     * Provides access to files and folders on a file system. You'll typically not instantiate this directly; instead you'll use an instance of one that has already been created for you. This class is abstract, meaning that you'll need to provide your own implementation in order to use it effectively.
+     */
+    declare export static class FileSystemProvider {
+        /**
+         * Indicates that this is a {@link FileSystemProvider}. Useful for type-checking.
+         */
         public readonly isFileSystemProvider: boolean;
+        /**
+         * An array of the domains this file system supports. If the file system can open a file picker to the user's `documents` folder, for example, then `userDocuments` will be in this list.
+         */
         public readonly supportedDomains: Symbol[];
 
-        public getFileForOpening(options: GetFileForOpeningOptions = {
-            allowMultiple: false,
-            types: ['.*']
-        }): Promise<File[]>;
+        /**
+         * Gets a file (or files) from the file system provider for the purpose of opening them. Files are read-only.
+         *
+         * Multiple files can be returned if the `allowMultiple` option is `true`.
+         * @param {object} options
+         * @param {Symbol} options.initialDomain the preferred initial location of the file picker. If not defined, the most recently used domain from a file picker is used instead.
+         * @param {string[]=['.*']} options.types the allowed file types
+         * @param {boolean=false} options.allowMultiple if `true`, multiple files can be returned (as an array)
+         *
+         * @returns the selected files, or empty if no file were selected.
+         */
+        public getFileForOpening(options?): Promise<File[]>;
 
-        public getFileForSaving(options: GetFileForSavingOptions): Promise<File>;
+        /**
+         * Gets a file reference suitable for saving. The file is read-write. Any file picker displayed will be of the "save" variety.
+         *
+         * If the user attempts to save a file that doesn't exist, the file is created automatically.
+         *
+         * If the act of writing to the file would overwrite it, the file picker should prompt the user if they are OK with that action. If not, the file should not be returned.
+         *
+         * @param {object} options
+         * @param {Symbol} options.initialDomain the preferred initial location of the file picker. If not defined, the most recently used domain from a file picker is used instead.
+         *
+         * @returns the selected file, or `null` if no file were selected.
+         */
+        public getFileForSaving(options?): Promise<File>;
 
+        /**
+         * Gets a folder from the file system via a folder picker dialog. The files and folders within can be accessed via {@link Folder.getEntries}. Any files within are read-write.
+         *
+         * If the user dismisses the picker, `null` is returned instead.
+         *
+         * @param {object} options
+         * @param {Symbol} options.initialDomain the preferred initial location of the file picker. If not defined, the most recently used domain from a file picker is used instead.
+         *
+         * @returns the selected folder, or `null` if no folder is selected.
+         */
         public getFolder(options: GetFolderOptions): Promise<Folder>;
 
+        /**
+         * Returns a temporary folder. The contents of the folder will be removed when the extension is disposed.
+         */
         public getTemporaryFolder(): Promise<Folder>;
 
+        /**
+         * Returns a folder that can be used for extension's data storage without user interaction. It is persistent across host-app version upgrades.
+         */
         public getDataFolder(): Promise<Folder>;
 
+        /**
+         * Returns an plugin's folder – this folder and everything within it are read only. This contains all the Plugin related packaged assets.
+         */
         public getPluginFolder(): Promise<Folder>;
 
+        /**
+         * Returns the fs url of given entry.
+         * @param entry the entry
+         */
         public getFsUrl(entry: Entry): string;
 
+        /**
+         * Returns the platform native file system path of given entry.
+         * @param entry the entry
+         */
         public getNativePath(entry: Entry): string;
 
+        /**
+         * Checks if the supplied object is a {@link FileSystemProvider}. It's safe to use even if the object is `null` or `undefined`. Useful for type checking.
+         * @param fs the object to check
+         * @returns If `true`, the object is a file system provider
+         */
         public static isFileSystemProvider(fs: any): boolean;
     }
 
