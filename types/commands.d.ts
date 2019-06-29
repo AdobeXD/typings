2b3ea03c46968788d6e59b1cb22a04f788a28a85
Pablo Klaschka <contact@pabloklaschka.de>
2019-06-29 17:44:32 +0200

    - Fixed `commands` "class"

--- types/commands.d.ts
+++ types/commands.d.ts
@@ -1,26 +1,26 @@
 /**
  * You can make structural changes to the scenegraph, and perform other complex operations, by programmatically invoking the same commands as XD users have access to in the UI. These methods do not take arguments. Instead, set the selection to the objects you want the command to target, then invoke the command:
  */
-declare class commands {
+declare module commands {
     /**
      * Wraps the selected objects in a Group, leaving the Group selected afterward. Equivalent to Object > Group in the UI.
      */
-    static group(): void;
+    function group(): void;
 
     /**
      * Ungroups any of the selected objects that are ungroupable containers (Group, SymbolInstance, RepeatGrid, etc.). Equivalent to _Object > Ungroup_.
      */
-    static ungroup(): void;
+    function ungroup(): void;
 
     /**
      * Creates a masked Group from the selected objects, using the object that is highest in the z order as the mask shape. The mask shape must be a leaf node or Boolean Group. Equivalent to Object > Mask With Shape.
      */
-    static createMaskGroup(): void;
+    function createMaskGroup(): void;
 
     /**
      * Converts each selected object to a Path with the exact same visual appearance. Only applies to leaf nodes and Boolean Groups. Equivalent to Object > Path > Convert to Path.
      */
-    static convertToPath(): void;
+    function convertToPath(): void;
 
     /**
      * Duplicates all selected objects, leaving the duplicates selected afterward.
@@ -30,83 +30,83 @@ declare class commands {
      *
      * Edit > Duplicate
      */
-    static duplicate(): void;
+    function duplicate(): void;
 
     /**
      * Brings selected objects to the front of the z order. Equivalent to Object > Arrange > Bring to Front.
      */
-    static bringToFront(): void;
+    function bringToFront(): void;
 
     /**
      * Brings each selected object one step closer to the front of the z order. Equivalent to Object > Arrange > Bring Forward.
      */
-    static bringForward(): void;
+    function bringForward(): void;
 
     /**
      * Sends selected objects to the back of the z order. Equivalent to Object > Arrange > Send to Back.
      */
-    static sendToBack(): void;
+    function sendToBack(): void;
 
     /**
      * Sends each selected object one step closer to the back of the z order. Equivalent to Object > Arrange > Send Backward.
      */
-    static sendBackward(): void;
+    function sendBackward(): void;
 
     /**
      * Aligns all selected objects flush left. Equivalent to Object > Align > Left.
      */
-    static alignLeft(): void;
+    function alignLeft(): void;
 
     /**
      * Aligns all selected objects flush right. Equivalent to Object > Align > Right.
      */
-    static alignRight(): void;
+    function alignRight(): void;
 
     /**
      * Aligns all selected objects along their horizontal centerlines. Equivalent to Object > Align > Center (Horizontally).
      */
-    static alignHorizontalCenter(): void;
+    function alignHorizontalCenter(): void;
 
     /**
      * Aligns all selected objects flush top. Equivalent to Object > Align > Top.
      */
-    static alignTop(): void;
+    function alignTop(): void;
 
     /**
      * Aligns all selected objects flush bottom. Equivalent to Object > Align > Bottom.
      */
-    static alignBottom(): void;
+    function alignBottom(): void;
 
     /**
      * Aligns all selected objects along their vertical centerlines. Equivalent to Object > Align > Center (Vertically).
      */
-    static alignVerticalCenter(): void;
+    function alignVerticalCenter(): void;
 
     /**
      * Distributes all selected objects evenly along the X axis. Equivalent to Object > Distribute > Horizontally.
      */
-    static distributeHorizontal(): void;
+    function distributeHorizontal(): void;
 
     /**
      * Distributes all selected objects evenly along the Y axis. Equivalent to Object > Distribute > Vertically.
      */
-    static distributeVertical(): void;
+    function distributeVertical(): void;
 
     /**
      * Shifts all selected objects and their content so they align crisply with the pixel grid. Equivalent to Object > Align to Pixel Grid.
      */
-    static alignToPixelGrid(): void;
+    function alignToPixelGrid(): void;
 
 
     // /**
     //* Flips the object horizontally. Some objects such as Symbols cannot be flipped. Equivalent to Object > Flip > Horizontally.
     //*/
-    //static filpHorizontal(): void;
+    //function filpHorizontal(): void;
 //
     //  /**
     // * Flips the object vertically. Some objects such as Symbols cannot be flipped. Equivalent to Object > Flip > Vertically.
     //*/
-    //static flipVertical(): void;
+    //function flipVertical(): void;
 }
 
 export = commands;
