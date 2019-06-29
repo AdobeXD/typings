63574ec279fc5fb441f72ab0c26a29886ea17e23
Pablo Klaschka <contact@pabloklaschka.de>
2019-06-29 17:44:56 +0200

    - Fixed `clipboard` "class"

--- types/clipboard.d.ts
+++ types/clipboard.d.ts
@@ -1,12 +1,12 @@
 /**
  * This module lets you copy text to the clipboard. It is not yet possible to copy other content types, or to handle paste events.
  */
-declare class clipboard {
+declare module clipboard {
     /**
      * Write plain text to the clipboard.
      * @param text Will be automatically converted to string if a different type is passed
      */
-    static copyText(text: string | any): void;
+    function copyText(text: string | any): void;
 }
 
 export = clipboard;
