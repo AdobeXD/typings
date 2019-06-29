d663c5a3d9e4b386251f7f95f28243c7d59b0dc0
Pablo Klaschka <contact@pabloklaschka.de>
2019-06-29 17:41:12 +0200

    - Fixed `viewport` "class"

--- types/viewport.d.ts
+++ types/viewport.d.ts
@@ -4,16 +4,16 @@
  */
 import {SceneNode} from "scenegraph";
 
-declare class viewport {
+declare module viewport {
     /**
      * The current viewport bounds expressed in global coordinates.
      */
-    static readonly bounds: {x:number,y:number,width:number,height:number};
+    const bounds: {x:number,y:number,width:number,height:number};
 
     /**
      * The current viewport zoom factor (where 1.0 = 100% zoom, 0.5 = 50% zoom, etc.).
      */
-    static readonly zoomFactor: number;
+    const zoomFactor: number;
 
     /**
      * Smoothly pan the viewport to bring the entire given region into view. If the region is already fully in view, does nothing. If the given region is too large to fit entirely in view, it is simply centered (zoom remains unchanged).
@@ -21,7 +21,7 @@ declare class viewport {
      * The region can be defined by passing a {@link SceneNode}, or by explicitly specifying a rectangle in global coordinates.
      * @param {!SceneNode} node The node that gets scrolled into view
      */
-    static scrollIntoView(node: SceneNode): void;
+    function scrollIntoView(node: SceneNode): void;
 
     /**
      * Smoothly pan the viewport to bring the entire given region into view. If the region is already fully in view, does nothing. If the given region is too large to fit entirely in view, it is simply centered (zoom remains unchanged).
@@ -32,14 +32,14 @@ declare class viewport {
      * @param width
      * @param height
      */
-    static scrollIntoView(x: number, y:number, width:number, height:number): void;
+    function scrollIntoView(x: number, y:number, width:number, height:number): void;
 
     /**
      * Smoothly pan the viewport to center on a specific point in the document's global coordinates. Even if the point is already in view, the view pans until it is centered.
      * @param {number} x The x-coordinate of the centered point (in the document's global coordinates)
      * @param {number} y The y-coordinate of the centered point (in the document's global coordinates)
      */
-    static scrollToCenter(x: number, y:number): void;
+    function scrollToCenter(x: number, y:number): void;
 
     /**
      * Zoom & pan the view as needed so the given region fills the viewport (with some padding). If the region is large, zooms out as needed so it fits entirely in view. If the region is smaller, zooms in so the region fills the entire viewport - may zoom in past 100% to achieve this.
@@ -47,7 +47,7 @@ declare class viewport {
      * The region can be defined by passing a {@link SceneNode}, or by explicitly specifying a rectangle in global coordinates.
      * @param {!SceneNode} node
      */
-    static zoomToRect(node: SceneNode): void;
+    function zoomToRect(node: SceneNode): void;
 
     /**
      * Zoom & pan the view as needed so the given region fills the viewport (with some padding). If the region is large, zooms out as needed so it fits entirely in view. If the region is smaller, zooms in so the region fills the entire viewport - may zoom in past 100% to achieve this.
@@ -58,7 +58,7 @@ declare class viewport {
      * @param {number} width
      * @param {number} height
      */
-    static zoomToRect(x: number, y:number, width:number, height:number): void;
+    function zoomToRect(x: number, y:number, width:number, height:number): void;
 }
 
 export = viewport;
