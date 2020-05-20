73fcf51e91b98dbab967d05158207bfacba9c1a6
Pablo Klaschka <contact@pabloklaschka.de>
2020-05-20 15:27:40 +0200

    application module updates

--- types/application.d.ts
+++ types/application.d.ts
@@ -48,6 +48,17 @@ type RenditionResult = {
     outputFile: File;
 }
 
+type DocumentInfo = {
+    /**
+     * Document name as displayed in the titlebar. For untitled documents, this will be a localized string such as "Untitled-1."
+     */
+    name: string,
+    /**
+     * *Semi*-unique document identifier. Duplicating an .xd file on disk will result in two files with the same GUID. Duplicating a document via "Save As" will change its GUID; thus two *cloud* documents will never have the same GUID. The GUID of an Untitled document doesn't change when it is saved for the first time. <br><br>This returns the same value as `scenegraph.root.guid`.
+     */
+    guid: string
+};
+
 /**
  * Generate renditions of nodes in the document in a batch. Overwrites any existing files without warning.
  *
@@ -72,3 +83,21 @@ export const appLanguage: string;
  * User's OS-wide locale setting. May not match the XD UI, since XD does not support all world languages. Includes both language and region (e.g. "fr_CA" or "en_US").
  */
 export const systemLocale: string;
+
+/**
+ * Information about the document which this instance of the plugin is attached to.
+ *
+ * > **Tip**
+ * >
+ * > _This does **not** indicate the frontmost "active" document window in the XD application._
+ * >
+ * > In XD, each document window loads a separate copy of your plugin. When a given instance of your plugin calls this API, you will always receive information about the document that this instance of the plugin is attached to, even if it's not the active window.
+ *
+ * @example ```js
+let application = require("application");
+let documentInfo = application.activeDocument;
+console.log("Document title: " + documentInfo.name);
+console.log("Document ID: " + documentInfo.guid);
+ ```
+ */
+export const activeDocument: DocumentInfo;
