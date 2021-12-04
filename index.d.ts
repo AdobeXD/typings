0cb8acbd19d3cf04512c3211ebac5e73c78b3284
James Bradford <j.bradford@pnnl.gov>
2021-12-04 13:22:56 -0800

    prep to be pushed to DefinitelyTyped

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,11 @@
+// Type definitions for Adobe XD and UXP v45.0.0
+// Project: https://www.adobe.io/xd/uxp/develop/reference/xd-index/
+// Definitions by: James Bradford <https://github.com/arniebradfo/>
+//                 Pablo Klaschka <https://github.com/pklaschka/>
+//                 Eric Robinson <https://github.com/ericdrobinson/>
+//                 Kerri Shotts <https://github.com/kerrishotts/>
+//                 Jonathan Fontanez <https://github.com/tato123/>
+// Definitions: https://github.com/AdobeXD/typings
+
 import './xd'
 import './uxp'
